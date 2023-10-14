bd2462ec7ad83e6ba69ed5fe36ef9ae338d34222
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-10-13 21:29:16 -0300

    organize

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //the package.json has the type: "module", cause without it, the nodejs cant run import and export methods
 
-import { binaryConverter } from "./binary.js"
-import { octalConverser } from "./octal.js"
-import { hexadecConverter } from "./hexadecimal.js"
+import { binaryConverter } from "./systems/binary.js"
+import { octalConverser } from "./systems/octal.js"
+import { hexadecConverter } from "./systems/hexadecimal.js"
 
 const number = 20425 //put any number you want
 const formatedNumber = Intl.NumberFormat('pt-BR').format(number) //language-sensitive number formatting (for Brazilian Portuguese)
